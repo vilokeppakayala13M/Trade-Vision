5b6cdc09216c73a09d760e0377d6f0ad7d50b122
vilokeppakayala13M <vilokeppakayala53@gmail.com>
2026-04-22 14:47:42 +0530

    removed debug and log files

--- debug-yf.js
+++
@@ -1,8 +0,0 @@
-const pkg = require('yahoo-finance2');
-console.log('Package Keys:', Object.keys(pkg));
-if (pkg.YahooFinance) {
-    console.log('pkg.YahooFinance exists, type:', typeof pkg.YahooFinance);
-} else {
-    console.log('pkg.YahooFinance MISSING');
-}
-console.log('pkg.default type:', typeof pkg.default);
