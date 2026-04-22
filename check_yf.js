758fd67efbd03653abf2ffb3bfd76651b4b82c19
vilokeppakayala13M <vilokeppakayala53@gmail.com>
2026-04-22 14:53:29 +0530

    removed test and debug files

--- check_yf.js
+++
@@ -1,9 +0,0 @@
-const yf = require('yahoo-finance2');
-console.log('Default export:', yf);
-console.log('Keys:', Object.keys(yf));
-try {
-    const instance = new yf.YahooFinance();
-    console.log('Instantiated successfully');
-} catch (e) {
-    console.log('Instantiation failed:', e.message);
-}
